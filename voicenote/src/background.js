6582d2f6e2d023f24905911c26f38867b9a1d7ae
Kory Prince <korylprince@gmail.com>
2019-03-26 10:34:01 -0500

    set language to en-US automatically

--- voicenote/src/background.js
+++ voicenote/src/background.js
@@ -1,5 +1,6 @@
 blrdRegexp = /^https:\/\/voicenote\.in\/.*$/
 
+// block everything but main site
 var callback = function(details) {
     if (!blrdRegexp.test(details.url)) {
         return {cancel: true};
@@ -21,15 +22,26 @@ chrome.app.runtime.onLaunched.addListener(function() {
             var webview = this.querySelector('#root');
             webview.request.onBeforeRequest.addListener(callback, filter, opt_extraInfoSpec);
             webview.addEventListener('contentload', function() {
+                // insert print CSS
                 webview.insertCSS({file: "print.css"});
+                // copy title
                 webview.executeScript({code: 'document.title'}, function(results) {
                     window.contentWindow.document.title = results[0];
                 });
+                // set language to en-US
+                webview.executeScript({code: `
+                    var storage = JSON.parse(window.localStorage.getItem("set"));
+                    if (!(storage["e"])) { storage["e"] = {}; }
+                    storage["e"]["o"] = "en-US";
+                    window.localStorage.setItem("set", JSON.stringify(storage));
+                    `});
             });
             webview.addEventListener("permissionrequest", function(e) {
+                // allow microphone
                 if (e.permission === "media") {
                     e.request.allow();
                 }
+                // override save button with print dialog
                 if (e.permission === "download") {
                     webview.print();
                 }
