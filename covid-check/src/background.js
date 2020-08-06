a23bee64b527748804c241e802734624d3796ea1
Kory Prince <korylprince@gmail.com>
2020-08-05 22:50:53 -0500

    update covid-check to 1.0.1

--- covid-check/src/background.js
+++ covid-check/src/background.js
@@ -1,16 +1,21 @@
+chrome.power.requestKeepAwake("display")
+
 chrome.app.runtime.onLaunched.addListener(function() {
     chrome.app.window.create("window.html", function(window) {
         window.contentWindow.document.addEventListener("DOMContentLoaded", function() {
             const webview = this.querySelector("#root");
-            var location_id
-            try {
-                location_id = chrome.storage.managed.get("location_id")
-            } catch {
-                const body = this.querySelector("body");
-                body.innerHTML = "location_id not set. Please contact your system administrator."
+            const body = this.querySelector("body");
+            if (chrome.storage == null || chrome.storage.managed == null) {
+                body.innerHTML = "<h1>Can't access chrome managed storage. Please contact your system administrator.</h1>"
                 return
             }
-            webview.src = `https://covid-check.bullardisd.net/client?location_id=${location_id}`
+            chrome.storage.managed.get("location_id", data => {
+                if (data.location_id != null) {
+                    webview.src = `https://covid-check.bullardisd.net/client?location_id=${data.location_id}`
+                    return
+                }
+                body.innerHTML = "<h1>location_id not set. Please contact your system administrator.</h1>"
+            })
         })
     })
 })
