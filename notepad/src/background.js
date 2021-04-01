9901d30f939ca9e110b609a8e17bc5bb7d1f46b5
Kory Prince <korylprince@gmail.com>
2021-04-01 15:32:53 -0500

    added notepad app

---
+++ notepad/src/background.js
@@ -0,0 +1,12 @@
+chrome.app.runtime.onLaunched.addListener(() => {
+    chrome.app.window.create('window.html', (window) => {
+        window.contentWindow.document.addEventListener('DOMContentLoaded', () => {
+            window.contentWindow.document.querySelector("#print").addEventListener("click", () => {
+                const text = window.contentWindow.document.querySelector("#text")
+                const printtext = window.contentWindow.document.querySelector("#printtext")
+                printtext.innerHTML = text.value
+                window.contentWindow.print()
+            })
+        })
+    })
+})
